e2b8ece4b9beba5bc2162a787ed024a4a49607f4
thesave <Thesave86@gmail.com>
2024-09-18 10:25:47 +0200

    Add files via upload

---
+++ plaintext_to_json_report_converter.rb
@@ -0,0 +1,16 @@
+require 'json'
+
+Dir.glob("*.report") do | file |
+  json = []
+  File.open( file, 'r' ).each_line do | line |
+    line = line.split(",")
+    json.push( { path: line[0].strip() , checksum: line[1].strip() } )
+  end
+  filename = File.basename( file, ".report" )
+  filename = "#{filename}.json"
+  output = JSON.pretty_generate( json )
+  File.open( "json/#{filename}", "w" ) do | f |
+    f.write( output )
+  end
+end
+
