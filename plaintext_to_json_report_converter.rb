c6bc570c780a8a7079fda0652374556ccf543cf3
Simone-Melloni <77337687+Simone-Melloni@users.noreply.github.com>
2024-09-20 15:45:22 +0200

    folder

--- plaintext_to_json_report_converter.rb
+++ plaintext_to_json_report_converter.rb
@@ -1,12 +1,12 @@
 require 'json'
 
-Dir.glob("*.report") do | file |
+Dir.glob("*.base") do | file |
   json = []
   File.open( file, 'r' ).each_line do | line |
     line = line.split(",")
     json.push( { path: line[0].strip() , checksum: line[1].strip() } )
   end
-  filename = File.basename( file, ".report" )
+  filename = File.basename( file, ".base" )
   filename = "#{filename}.json"
   output = JSON.pretty_generate( json )
   File.open( "json/#{filename}", "w" ) do | f |
